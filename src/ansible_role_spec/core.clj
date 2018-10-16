1849fea4163f6c54f09b9a435e97da20d8becf8c
kazesberger <klaus.azesberger@bearingpoint.com>
2018-10-16 11:49:10 +0200

    wip - some inline comments

--- src/ansible_role_spec/core.clj
+++ src/ansible_role_spec/core.clj
@@ -5,10 +5,11 @@
   (:import (java.io File)))
 
 
+; for now hardcode project-path to our local sample_project
 (def project-path "sample_project")
 
 (defn parse-yaml-file [file]
-  (println (str project-path File/separator file))
+  (println (str "parsing: " project-path File/separator file))
   (yaml/parse-string (slurp (str project-path File/separator file))))
 
 ; config contains use cases to validate
@@ -25,10 +26,28 @@
     ;TODO extract relevant plays from playbooks-code while considering limit and inventory config
     [test-description playbook inventories limit]))
 
-(get-relevant-plays cfg)
+; projects have use cases to test (defined in ars.yml)
+;;; use cases are essentially ansible-playbook calls with various paramaters
+; use cases have plays (among params etc.)
+
+(def relevant-plays (get-relevant-plays cfg))
+
+; plays have hosts
+; hosts have var map
+
+; plays have roles
+; roles have specs
+
+; in order to validate role specs they need the specific var map they would get passed in a real ansible-playbook run
+
+(get-specs relevant-plays)
 
 (comment
-  (watching/watch! {} [project-path] #(println "foo" %&))
+  (def watches (atom #{}))
+  (swap! watches conj (watching/watch! {} [project-path] #(println "foo" %&)))
+  (watching/stop! (first @watches))
+                
+
   ((into {} cfg) :playbook)
   ((first cfg) :test-description)
   ((second cfg) :test-description))
